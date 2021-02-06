31a47de08b30ab3dc9e8ba7e49512daf15a7ab8b
Heru Purwito <herupurwitodotcom@gmail.com>
2021-02-06 07:49:06 +0700

    fix sql error: #1824 - Failed to open the referenced table 'province'

--- wilayah_indonesia.sql
+++ wilayah_indonesia.sql
@@ -11,6 +11,53 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
+-- Dumping structure for table wilayah.province
+CREATE TABLE IF NOT EXISTS `province` (
+  `province_id` int(11) NOT NULL,
+  `province` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`province_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- Dumping data for table wilayah.province: ~34 rows (approximately)
+DELETE FROM `province`;
+/*!40000 ALTER TABLE `province` DISABLE KEYS */;
+INSERT INTO `province` (`province_id`, `province`) VALUES
+	(1, 'Bali'),
+	(2, 'Bangka Belitung'),
+	(3, 'Banten'),
+	(4, 'Bengkulu'),
+	(5, 'DI Yogyakarta'),
+	(6, 'DKI Jakarta'),
+	(7, 'Gorontalo'),
+	(8, 'Jambi'),
+	(9, 'Jawa Barat'),
+	(10, 'Jawa Tengah'),
+	(11, 'Jawa Timur'),
+	(12, 'Kalimantan Barat'),
+	(13, 'Kalimantan Selatan'),
+	(14, 'Kalimantan Tengah'),
+	(15, 'Kalimantan Timur'),
+	(16, 'Kalimantan Utara'),
+	(17, 'Kepulauan Riau'),
+	(18, 'Lampung'),
+	(19, 'Maluku'),
+	(20, 'Maluku Utara'),
+	(21, 'Nanggroe Aceh Darussalam (NAD)'),
+	(22, 'Nusa Tenggara Barat (NTB)'),
+	(23, 'Nusa Tenggara Timur (NTT)'),
+	(24, 'Papua'),
+	(25, 'Papua Barat'),
+	(26, 'Riau'),
+	(27, 'Sulawesi Barat'),
+	(28, 'Sulawesi Selatan'),
+	(29, 'Sulawesi Tengah'),
+	(30, 'Sulawesi Tenggara'),
+	(31, 'Sulawesi Utara'),
+	(32, 'Sumatera Barat'),
+	(33, 'Sumatera Selatan'),
+	(34, 'Sumatera Utara');
+/*!40000 ALTER TABLE `province` ENABLE KEYS */;
+
 -- Dumping structure for table wilayah.city
 CREATE TABLE IF NOT EXISTS `city` (
   `city_id` int(11) NOT NULL,
@@ -529,53 +576,6 @@ INSERT INTO `city` (`city_id`, `province_id`, `city_name`, `type`, `postal_code`
 	(500, 24, 'Yalimo', 'Kabupaten', 99481),
 	(501, 5, 'Yogyakarta', 'Kota', 55111);
 /*!40000 ALTER TABLE `city` ENABLE KEYS */;
-
--- Dumping structure for table wilayah.province
-CREATE TABLE IF NOT EXISTS `province` (
-  `province_id` int(11) NOT NULL,
-  `province` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`province_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- Dumping data for table wilayah.province: ~34 rows (approximately)
-DELETE FROM `province`;
-/*!40000 ALTER TABLE `province` DISABLE KEYS */;
-INSERT INTO `province` (`province_id`, `province`) VALUES
-	(1, 'Bali'),
-	(2, 'Bangka Belitung'),
-	(3, 'Banten'),
-	(4, 'Bengkulu'),
-	(5, 'DI Yogyakarta'),
-	(6, 'DKI Jakarta'),
-	(7, 'Gorontalo'),
-	(8, 'Jambi'),
-	(9, 'Jawa Barat'),
-	(10, 'Jawa Tengah'),
-	(11, 'Jawa Timur'),
-	(12, 'Kalimantan Barat'),
-	(13, 'Kalimantan Selatan'),
-	(14, 'Kalimantan Tengah'),
-	(15, 'Kalimantan Timur'),
-	(16, 'Kalimantan Utara'),
-	(17, 'Kepulauan Riau'),
-	(18, 'Lampung'),
-	(19, 'Maluku'),
-	(20, 'Maluku Utara'),
-	(21, 'Nanggroe Aceh Darussalam (NAD)'),
-	(22, 'Nusa Tenggara Barat (NTB)'),
-	(23, 'Nusa Tenggara Timur (NTT)'),
-	(24, 'Papua'),
-	(25, 'Papua Barat'),
-	(26, 'Riau'),
-	(27, 'Sulawesi Barat'),
-	(28, 'Sulawesi Selatan'),
-	(29, 'Sulawesi Tengah'),
-	(30, 'Sulawesi Tenggara'),
-	(31, 'Sulawesi Utara'),
-	(32, 'Sumatera Barat'),
-	(33, 'Sumatera Selatan'),
-	(34, 'Sumatera Utara');
-/*!40000 ALTER TABLE `province` ENABLE KEYS */;
 
 -- Dumping structure for table wilayah.subdistrict
 CREATE TABLE IF NOT EXISTS `subdistrict` (
